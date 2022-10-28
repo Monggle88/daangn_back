1424a1bb830eff87e5a8048ecc985fd9bcecf05c
[Github <[Github>
2022-10-29 00:07:29 +0900

    첫 커밋!!!!

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
-const port = 4000;
-const env = require('./.env');
 const routes = require('./index');
 
 app.use('/', routes); // 라우터 등록
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
     console.log("서버를 가동합니다");
   });
