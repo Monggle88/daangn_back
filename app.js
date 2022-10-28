aae0531f51a9adc111f5a149d5c28ccae1680621
[Github <[Github>
2022-10-28 21:47:43 +0900

    ^-^

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("test")
+const express = require("express");
+const app = express();
+const port = 4000;
+const env = require('./.env');
+const routes = require('./index');
+
+app.use('/', routes); // 라우터 등록
+
+app.listen(port, () => {
+    console.log("서버를 가동합니다");
+  });
