3a2251c8a885a409adadb184cee7db1ffe82c859
[Github <[Github>
2022-10-29 00:11:11 +0900

    브뤤취 테스트

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const routes = require('./index');
 
-app.use('/', routes); // 라우터 등록
+app.use('/', routes); // 라우터 등록 브렌ㅊ추이ㅣ이이두부
 
 app.listen(process.env.PORT, () => {
     console.log("서버를 가동합니다");
