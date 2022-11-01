856f403643ce618aa7b011f7a53d0a09aa6da1d2
Monggle88 <fmonggle88@gmail.com>
2022-11-01 21:04:00 +0900

    login fix

--- middlewares/authLoginUserMiddleware.js
+++ middlewares/authLoginUserMiddleware.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
+
 module.exports = async (req, res, next) => {
   try {
-    console.log('32432423423')
-    if (req.headers.authorization || req.cookies) {
-      console.log('99999999999999999999999')
+    console.log(req.headers.authorization);
+    console.log(req.cookies.Authorization);
+
+    if (req.headers.authorization || req.cookies.Authorization) {
       return res.status(400).json({
         ok: false,
         errorMessage: '이미 로그인이 되어있습니다.',
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,24 +1,28 @@
-const jwt = require("jsonwebtoken");
-const {Users} = require("../models");
+const jwt = require('jsonwebtoken');
+const { Users } = require('../models');
 
 module.exports = async (req, res, next) => {
-    try{
-        const cookie = req.headers.authorization || req.cookies
+  try {
+    const cookie = req.headers.authorization || req.cookies.authorization;
 
-        if(!cookie){
-            return res.status(400).json({ ok:false, errorMessage:"로그인 후 이용 가능합니다1111" });
-        }
-        const [tokenType, tokenValue] = cookie.split(' ');
-        if(tokenType !== 'Bearer'){
-            return res.status(400).json({ ok:false, errorMessage:"쿠키에 오류가 발생했습니다" });
-        }
-
-        const {userId} = jwt.verify(tokenValue, 'secret_dang'); // 궁금
-        const user = await Users.findByPk(userId);
-        res.locals.user = user;
-        next()
-    }catch(error){
-        console.log(error);
-        return res.status(400).json({ ok:false, errorMessage:"로그인 후 이용 가능합니다" });
+    if (!cookie) {
+      return res
+        .status(400)
+        .json({ ok: false, errorMessage: '로그인 후 이용 가능합니다.' });
     }
+
+    const { userId } = jwt.verify(cookie, 'secret_dang'); // 궁금
+    const user = await Users.findByPk({
+      attributes: ['email', 'password'],
+      where: { userId },
+    });
+    res.locals.user = user;
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(400)
+      .json({ ok: false, errorMessage: '로그인 후 이용 가능합니다' });
+  }
 };
