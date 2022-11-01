e68ed9ef9c8894244800efae2707c66f57cc2a81
Monggle88 <fmonggle88@gmail.com>
2022-11-02 00:09:43 +0900

    fix login

--- middlewares/authLoginUserMiddleware.js
+++ middlewares/authLoginUserMiddleware.js
@@ -2,10 +2,7 @@ require('dotenv').config();
 
 module.exports = async (req, res, next) => {
   try {
-    console.log(req.headers.authorization);
-    console.log(req.cookies.Authorization);
-
-    if (req.headers.authorization || req.cookies.Authorization) {
+    if (req.headers.Authorization || req.cookies.Authorization) {
       return res.status(400).json({
         ok: false,
         errorMessage: '이미 로그인이 되어있습니다.',
--- user/user.controller.js
+++ user/user.controller.js
@@ -31,7 +31,7 @@ class UserController {
     // if(!loginUser){
     //     return res.status(400).json({ ok: false, errorMessage:"이메일 혹은 비밀번호를 확인해 주세요" })
     // }  service에서 했음 프론트랑 얘기해 보고 수정
-    res.header('Authorization', loginUser);
+    res.headers('Authorization', loginUser);
     // res cookie
     res.cookie('token', loginUser);
     res.cookie('Authorization', loginUser, { Expires: 3600 });
