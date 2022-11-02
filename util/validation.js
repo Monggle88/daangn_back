749ea1c6f87b68b94c73f6152b1f347a5f22f3f0
JiHyeunM <moon011011@gmail.com>
2022-11-03 05:18:20 +0900

    중복 검사 완료

--- util/validation.js
+++ util/validation.js
@@ -9,7 +9,7 @@ module.exports = {
         /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/
       )
       .required(),
-    nickname: joi.string().min(2).max(8).required(),
+    nickname: joi.string().min(2).max(10).required(),
     password: joi
       .string()
       .pattern(/^(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,16}$/)
@@ -23,7 +23,7 @@ module.exports = {
     email: joi
       .string()
       .pattern(
-        /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
+        /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/
       )
       .required(),
     password: joi
@@ -31,4 +31,15 @@ module.exports = {
       .pattern(/^(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,16}$/)
       .required(),
   }),
+  emailDupSchema: joi.object({
+    email: joi
+    .string()
+    .pattern(
+        /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/
+      )
+    .required()
+  }),
+  nicknameDupSchema: joi.object({
+    nickname: joi.string().min(2).max(10).required()
+  })
 };
